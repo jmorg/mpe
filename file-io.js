07885e216a7bf8128a015dd7946cb67cc6ba1c4b
Kimberly Wijaya <kcwijaya@stanford.edu>
2017-01-18 01:29:38 -0800

    Got file upload working

--- file-io.js
+++ file-io.js
@@ -1,4 +1,26 @@
- $(document).ready(function(){
+   $(document).ready(function(){
+
+        var socket = io();
+        socket.on('get file', function(path) {
+            $("#messages").append('<li><img style="width: 25%; height: 25%" src="' + path +'"></img></li>');
+            $(".close-button").click();
+        });
+        $(".close-button").click(function(){
+            $(".pop-up").hide();
+        });
+
+        $('#share-image').click(function(){
+            $("#share-image-pop").show();
+        });
+
+        $('#share-video').click(function(){
+            $("#share-video-pop").show();
+        });
+
+        $('#share-voice').click(function(){
+            $("#share-voice-pop").show();
+        });
+
         $('#share-image').hover(
             function(){
                 $("#share-image-help").show();
--- server.js
+++ server.js
@@ -1,26 +1,98 @@
 var app = require('express')();
+var express = require('express');
+var multer = require('multer');
+var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+var fs = require('fs');
+var dl = require('delivery');
+var upload = multer({dest: __dirname + '/uploadedFiles/'});
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
  });
+
 app.get('/main.js', function(req, res) {
     res.sendFile(__dirname + '/main.js');
  });
 
 
-io.on('connection', function(socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-    });
-    socket.on('chat message', function(msg) {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
-   
+app.get('/main.css', function(req, res) {
+    res.sendFile(__dirname + '/main.css');
+ });
+
+app.get('/file-io.js', function(req, res){
+    res.sendFile(__dirname+'/file-io.js');
 });
+
+app.get('/uploadedFiles/*', function(req, res){
+    res.sendFile(__dirname + req.url);
+});
+
+app.get("/file_upload", function(req, res){
+    res.json({message: "Blah"});
+});
+
+app.post('/file_upload', upload.single('file'), function(req, res, next) {
+  var file = 'uploadedFiles/' + req.file.filename +".jpg";
+  fs.rename(req.file.path, file, function(err) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    } else {
+        io.emit('get file', file);
+      // res.send({file: req.file.path});
+    }
+  });
+});
+
+// io.sockets.on('connection', function(socket){
+//   var delivery = dl.listen(socket);
+//   delivery.on('delivery.connect',function(delivery){
+ 
+//     delivery.send({
+//       name: 'sample-image.jpg',
+//       path : './sample-image.jpg',
+//       params: {foo: 'bar'}
+//     });
+ 
+//     delivery.on('send.success',function(file){
+//       console.log('File successfully sent to client!');
+//     });
+ 
+//   });
+// });
+
+// io.on('connection', function(socket) {
+//     console.log('a user connected');
+//     socket.on('disconnect', function() {
+//         console.log('user disconnected');
+//     });
+
+//     var delivery = dl.listen(socket);
+//     delivery.on('receive.success',function(file){
+//     var params = file.params;
+//     fs.writeFile(file.name,file.buffer, function(err){
+//       if(err){
+//         console.log('File could not be saved.');
+//       }else{
+//         console.log('File saved.');
+//       };
+//     });
+//   });
+
+//     socket.on('chat message', function(msg) {
+//         console.log('message: ' + msg);
+//         io.emit('chat message', msg);
+//     });
+   
+// });
+
+
 
 
 http.listen(process.env.PORT || 5000, function() {
