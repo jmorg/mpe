e340e8f317e635c4c9fdf989aba1d4fa77b7c781
Hilary Sun <Hilary Sun>
2017-01-18 10:29:28 -0800

    Webcam access but not sure if peer to peer works?

--- main.js
+++ main.js
@@ -45,3 +45,28 @@ socket.on('chat message', function(msg) {
 socket.on('load local storage', function() {
     loadAllLocalStorage();
 });
+
+
+navigator.getUserMedia = navigator.getUserMedia ||
+    navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+var constraints = {
+  audio: false,
+  video: true
+};
+var video = document.querySelector('video');
+
+function successCallback(stream) {
+  window.stream = stream; // stream available to console
+  if (window.URL) {
+    video.src = window.URL.createObjectURL(stream);
+  } else {
+    video.src = stream;
+  }
+}
+
+function errorCallback(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);
--- server.js
+++ server.js
@@ -10,10 +10,21 @@ app.get('/main.js', function(req, res) {
     res.sendFile(__dirname + '/main.js');
 });
 
-app.get('/styles.css', function(req, res) {
-    res.sendFile(__dirname + '/styles.css');
+app.get('/morestyles.css', function(req, res) {
+    res.sendFile(__dirname + '/morestyles.css');
 });
 
+app.get('/othermain.js', function(req, res) {
+    res.sendFile(__dirname + '/othermain.js');
+});
+
+app.get('/common.js', function(req, res) {
+    res.sendFile(__dirname + '/common.js');
+});
+
+app.get('/ga.js', function(req, res) {
+    res.sendFile(__dirname + '/ga.js');
+});
 
 io.on('connection', function(socket) {
     io.emit('load local storage');
@@ -29,7 +40,7 @@ io.on('connection', function(socket) {
 });
 
 
-http.listen(process.env.PORT || 5000, function() {
+http.listen(3000, function() {
     console.log('listening on ' + process.env.PORT || 5000);
 });
 
