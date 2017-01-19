8d33ccf6bf6f67a3dafc777b16f21f84c3d00436
hsun083 <hsun083@gmail.com>
2017-01-18 22:58:14 -0800

    Changed the port number back

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ io.on('connection', function(socket) {
 });
 
 
-http.listen(3000, function() {
+http.listen(process.env.PORT || 5000, function() {
     console.log('listening on ' + process.env.PORT || 5000);
 });
 
